100c0c044f4fabf5e6363950df2d2ffba93db3b8
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-04-10 01:03:51 -0400

    added scss loaders, surge and everything else

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let secs = 0;
+setInterval(() => {
+  secs += 1;
+  $('#main').html(`You've been on this page for ${secs} seconds.`);
+}, 1000);
