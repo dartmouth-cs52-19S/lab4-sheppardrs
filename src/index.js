4bb7b504cf1e7e0041deade598d793ed5491db20
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-04-14 13:19:20 -0400

    added react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let secs = 0;
-setInterval(() => {
-  secs += 1;
-  $('#main').html(`You've been on this page for ${secs} seconds.`);
-}, 1000);
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
