fcab6c59a2515bd75bd3caebec8ecf3f8eba0015
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-04-25 11:43:54 -0400

    added react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import $ from 'jquery';
 import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
 
 let secs = 0;
 setInterval(() => {
