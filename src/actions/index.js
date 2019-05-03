4294b4b679e50cd4238c4c73a153257cf6cc7407
sheppardrs <sheppard.r.somers.19@dartmouth.edu>
2019-05-03 11:44:31 -0400

    uses heroku database now

--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,10 @@ export const ActionTypes = {
 
 // const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
 const API_KEY = '?key=s_somers';
-const ROOT_URL = 'http://localhost:9090/api';
+// local testing
+// const ROOT_URL = 'http://localhost:9090/api';
+// heroku database
+const ROOT_URL = 'https://cs52blogs.herokuapp.com/api';
 
 
 export function changePost(post) {
